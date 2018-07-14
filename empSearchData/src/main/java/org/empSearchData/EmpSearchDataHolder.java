b0eb770a4403c083f3435b171a462b204344699f
abhay khati <abhaykhati@gmail.com>
2018-07-14 19:20:02 +0530

    MavenMultiModuleEmpSearch

---
+++ empSearchData/src/main/java/org/empSearchData/EmpSearchDataHolder.java
@@ -0,0 +1,42 @@
+package org.empSearchData;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class EmpSearchDataHolder {
+	
+	static Map<String,Double> emp=new HashMap<String,Double>();
+	
+	public static  Map<String,Double>  searchEmpData(String empName) {
+		  
+		 emp.put("abhay", 50000d);
+		 emp.put("bhagyashree", 100000d);
+		 emp.put("prachi", 30000d);
+		 
+		 Set<Map.Entry<String, Double>> entrySet=emp.entrySet();
+		 
+		 
+		 for (Entry<String, Double> entry : entrySet) {
+			 
+			 if(entry.getKey().equals(empName)) {
+				 
+				 System.out.println("Employee Name "+ "  "+entry.getKey()+"  Employee Salary=   "+ entry.getValue());
+				 
+			 }
+		}
+		return emp;
+		
+	}
+	
+	
+/*	public static void main(String[] args) {
+
+		EmpSearchDataHolder.setEmpData("abhay");
+
+	}	
+*/
+
+}
---
+++ empSearchService/src/main/java/empSearchService/EmployeeSearchService.java
@@ -0,0 +1,13 @@
+package empSearchService;
+
+import org.empSearchData.EmpSearchDataHolder;
+
+public class EmployeeSearchService {
+
+	public static void main(String[] args) {
+
+		EmpSearchDataHolder.searchEmpData("bhagyashree");
+		
+	}
+
+}
